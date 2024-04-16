536c090b3e2cbc54e0f05f06c38418ea9c37d0c4
rajeh <rajeshramesh195@gmail.com>
2024-04-16 15:41:11 +0530

    combine

---
+++ src/com/employeeDetial/Primitive.java
@@ -0,0 +1,12 @@
+package com.employeeDetial;
+
+
+public class Primitive{
+  public static void main(String[]args) {
+     String s="viji  loves";
+     System.out.println(s);
+     String r=" bharathi";
+     System.out.println(r);
+     System.out.println(s+r);
+  }
+ }
